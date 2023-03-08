7dc7ff9baa98df2f7f5a7c47d1f2dd8b03f2f020
sillypoise <somewhatjose@gmail.com>
2023-03-08 17:24:50 -0500

    Add `react-router` to client app

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 function App() {
     return (
-        <main>
-            <article className="center">
+        <main className="center mlb-xl">
+            <article className="stack">
                 <h1 className="text-4">What do I eat?</h1>
+                <p>Explore and discover new recipes to cook!</p>
             </article>
         </main>
     );
